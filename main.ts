dd8e11274576b7ce255ff47d8492c1139653997c
theotheo <ibelyalov@yandex.ru>
2023-10-19 12:42:17 +0300

    Implement using moment.js for block id

--- main.ts
+++ main.ts
@@ -1,81 +1,65 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, HeadingCache, ListItemCache, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, SectionCache, Setting } from 'obsidian';
+import moment from "moment";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface SmartBlockSettings {
+	format: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: SmartBlockSettings = {
+	format: 'YYYYMMDDHHmmss'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class SmartBlock extends Plugin {
+	settings: SmartBlockSettings;
+
+	generateId() {
+		return moment().format(this.settings.format);
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			name: 'Set block id',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+				const fileCache = this.app.metadataCache.getFileCache(view.file);
+				const cursor = editor.getCursor("to");
+				let block: ListItemCache | HeadingCache | SectionCache = (
+					fileCache?.sections || []
+				  ).find((section) => {
+					return (
+					  section.position.start.line <= cursor.line &&
+					  section.position.end.line >= cursor.line
+					);
+				  });
+				new Notice(JSON.stringify(block))
+				const headings: HeadingCache[] = (fileCache?.headings || [])
+				let headingIndex = headings.findLastIndex((heading) => {
+					return (heading.position.start.line <= cursor.line);
+				  });
+				new Notice(JSON.stringify(headings[headingIndex]))
+				// console.log(editor.getSelection());
+				let blockId: string = block.id;
+				if (!blockId) {
+					blockId = this.generateId()
+					editor.replaceRange(` ^${blockId}`, block.position.end)
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
 				}
+				navigator.clipboard.writeText(blockId)
+				// editor.replaceSelection('Sample Editor Command');
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new SmartBlockSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+		// // Using this function will automatically remove the event listener when this plugin is disabled.
+		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+		// 	console.log('click', evt);
+		// });
 	}
 
 	onunload() {
@@ -90,27 +74,10 @@ export default class MyPlugin extends Plugin {
 		await this.saveData(this.settings);
 	}
 }
+class SmartBlockSettingTab extends PluginSettingTab {
+	plugin: SmartBlock;
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: SmartBlock) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,13 +88,13 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Block Id Format')
+			.setDesc('Moment.js-dateformat string')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('YYYYMMDDHHmmss')
+				.setValue(this.plugin.settings.format)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.format = value;
 					await this.plugin.saveSettings();
 				}));
 	}
